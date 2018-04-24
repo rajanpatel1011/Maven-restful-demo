37e6eb45278b113cd34596cd0c7a1e122d1bf6ba
Rajan Patel <rajanpatel1011@gmail.com>
2018-04-24 15:51:41 -0400

    implimented JAXRS injection resource
    used FormParam annotation
    for get http form value to java object

---
+++ src/main/java/com/org/JaxRSInjectionResource.java
@@ -0,0 +1,36 @@
+package com.org;
+
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("injection")
+public class JaxRSInjectionResource {
+
+	@GET
+	@Path("/store/{empId}/{ename}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String storeEmployee(@PathParam("empId")int id,@PathParam("ename") String name) {
+		return "empID"+id+"name"+name;
+	}
+	
+	@GET
+	@Path("/departments/{id}/employees")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getAllEmployeeByDepartment(@PathParam("id")int deptId,@QueryParam("designation")String desig) {
+		return "Department"+deptId+"designation"+desig;
+	}
+	
+	@POST
+	@Path("/store")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String storeEmp(@FormParam("n1")int deptid,@FormParam("n2") String name) {
+		return "Employee"+deptid+"with a name"+name+"stored";
+	}
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,7 +2,11 @@
 <body>
     <h2>Jersey RESTful Web Application!</h2>
     <p><a href="webapi/myresource">Jersey resource</a>
-    <p>Visit <a href="http://jersey.java.net">Project Jersey website</a>
-    for more information on Jersey!
+<hr/>
+<form action="webapi/injection/store" method ="post">
+Employee Id = <input type="text" name="n1">
+Employee Name = <input type="text" name="n2">
+<input type="submit" value="send" >
+</form>
 </body>
 </html>
